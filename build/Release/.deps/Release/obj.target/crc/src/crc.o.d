2f7336b87d431f3f1e0c9936408778bac29ac14f
lwawrzyniak <l.wawrzyniak@wawit.net>
2019-08-30 14:40:47 +0200

    Remove build from git index

--- build/Release/.deps/Release/obj.target/crc/src/crc.o.d
+++
@@ -1,7 +0,0 @@
-cmd_Release/obj.target/crc/src/crc.o := cc '-DNODE_GYP_MODULE_NAME=crc' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/lwawrzyniak/.cache/node-gyp/10.16.3/include/node -I/home/lwawrzyniak/.cache/node-gyp/10.16.3/src -I/home/lwawrzyniak/.cache/node-gyp/10.16.3/deps/openssl/config -I/home/lwawrzyniak/.cache/node-gyp/10.16.3/deps/openssl/openssl/include -I/home/lwawrzyniak/.cache/node-gyp/10.16.3/deps/uv/include -I/home/lwawrzyniak/.cache/node-gyp/10.16.3/deps/zlib -I/home/lwawrzyniak/.cache/node-gyp/10.16.3/deps/v8/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer  -MMD -MF ./Release/.deps/Release/obj.target/crc/src/crc.o.d.raw   -c -o Release/obj.target/crc/src/crc.o ../src/crc.c
-Release/obj.target/crc/src/crc.o: ../src/crc.c \
- /home/lwawrzyniak/.cache/node-gyp/10.16.3/include/node/node_api.h \
- /home/lwawrzyniak/.cache/node-gyp/10.16.3/include/node/node_api_types.h
-../src/crc.c:
-/home/lwawrzyniak/.cache/node-gyp/10.16.3/include/node/node_api.h:
-/home/lwawrzyniak/.cache/node-gyp/10.16.3/include/node/node_api_types.h:
